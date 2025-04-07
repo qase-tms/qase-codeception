6033e4042b42b450edec9bd001e88f144db3490d
Dmitrii Gridnev <dgridnev@qase.io>
2025-04-07 15:13:49 +0200

    feat: enhance Qase ID handling in reporter and attributes
    - Updated the reporter to support multiple Qase IDs through a new QaseIds attribute.
    - Modified the Metadata model to store an array of Qase IDs instead of a single ID.
    - Improved the AttributeParser to handle both single and multiple Qase IDs.
    - Added validation to ensure only integer values are accepted for Qase IDs.

--- src/Models/Metadata.php
+++ src/Models/Metadata.php
@@ -5,7 +5,7 @@ namespace Qase\Codeception\Models;
 class Metadata
 {
     public ?string $title = null;
-    public ?int $qaseId = null;
+    public ?array $qaseIds = [];
     public array $suites = [];
     public array $parameters = [];
     public array $fields = [];
